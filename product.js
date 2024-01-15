ddbc1449263ffd8929213ecf2ef8f83a9a4a4fc7
Mishra vibhakar <mishra.vibhakar7262@gmail.com>
2024-01-15 21:53:12 +0530

    done

---
+++ product.js
@@ -0,0 +1,8 @@
+// this js file is used to store the details about all the products
+const productList = [
+    {id: 1, name: "Product-1", price:100 , Quantity:0},
+    {id: 2, name: "Product-2", price:200,  Quantity:0},
+    {id: 3, name: "Product-3", price:300,  Quantity:0},
+    {id: 4, name: "Product-4", price:400,  Quantity:0}
+    
+];
---
+++ scipt.js
@@ -0,0 +1,78 @@
+ const main_card = document.querySelector("#product-cards-container");
+ const cart_card = document.querySelector("#cart-cards-cont");
+ const totalPrice = document.querySelector("#total-price");
+
+ const productList = [
+    {id: 1, name: "Product-1", price:100 , Quantity:0},
+    {id: 2, name: "Product-2", price:200,  Quantity:0},
+    {id: 3, name: "Product-3", price:300,  Quantity:0},
+    {id: 4, name: "Product-4", price:400,  Quantity:0}
+    
+];
+
+//productlist section
+
+
+function call(){
+    productList.forEach((prod) =>{
+        let cont = document.createElement("div");
+        cont.innerHTML = `
+        <p class = "product_name">${prod.name}</p>
+        <p class = "product_price">${prod.price}</p>
+        <div class = "count_section">
+            <p class ="minus" onclick="removetoCart(${prod.id})">-</p>
+            <p class ="count_${prod.id}">${prod.Quantity}</p>
+            <p class ="plus" onclick="addtoCart(${prod.id})">+</p>
+        </div>`
+        cont.classList.add("right_card");
+        main_card.appendChild(cont);
+
+    })
+}
+
+call();
+function addtoCart(prodId){
+    console.log(prodId);
+    const value = productList.find(p=> p.id === prodId);
+    console.log(value);
+    if(value){
+        value.Quantity+=1;
+        newfun(prodId);
+    }
+   
+}
+
+function removetoCart(prodId){
+    const value = productList.find(p=> p.id === prodId);
+    console.log(value);
+    if(value.Quantity>0){
+        if(value){
+            value.Quantity-=1;
+            newfun(prodId);
+        }
+        
+    }
+}
+
+
+function newfun(prodId){
+    const ele = document.querySelector(`.count_${prodId}`);
+    const value = productList.find(p=> p.id === prodId);
+
+    if(value && ele ){
+        ele.innerText = value.Quantity;
+    }
+}
+
+
+function updtCountAndCart(){
+    if(count>0){
+        let card = document.createElement("div");
+        card.innerHTML=`
+        <p class = "product_name">${prod.name}</p>
+        <p class = "product_price">${prod.price}</p>
+        <p class = "product_count">${count}</p>`
+        card.classList.add("card_cont");
+        cart_card.appendChild(card);
+    }
+}
